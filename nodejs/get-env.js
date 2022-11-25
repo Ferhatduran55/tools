f2851c13d3ed4c686209cc1612017c531575ff5c
Ferhat55 <ferhatduran2005@gmail.com>
2022-11-25 12:08:50 +0300

    nodejs

---
+++ nodejs/get-env.js
@@ -0,0 +1,24 @@
+function getEnv(properties, isInt = false) {
+    var output;
+    var arr = Object.entries(process.env);
+    arr.forEach(element => {
+        if (Array.isArray(element)) {
+            let num = 0;
+            element.forEach(element2 => {
+                if (element2 == properties || num >= 1) {
+                    num += 1;
+                    if (num == 2) {
+                        if (isInt) {
+                            output = parseInt(element2);
+                            exit;
+                        } else {
+                            output = element2;
+                            exit;
+                        }
+                    }
+                }
+            });
+        }
+    });
+    return output;
+}
